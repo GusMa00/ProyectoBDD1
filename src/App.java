c8af5d5be848b2a6738d3b2d5539a0a07f17d7a1
GusMa00 <bramirezm1501@alumno.ipn.mx>
2022-03-24 10:15:45 -0600

    Conexion exitosa

--- src/App.java
+++ src/App.java
@@ -7,12 +7,12 @@ import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Prueba");
         // Variable para la conexion 
-        String connectionUrl = "jdbc:sqlserver://IDEAPAD-GAMING-:1433;databaseName=AdventureWorks2019;user=sa;password=cacas;";
+        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks2019;user=sa;password=cacas;encrypt=true;trustServerCertificate=true";
 
         try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
-            String SQL = "SELECT TOP 10 * FROM Person.Contact";
+            String SQL = "SELECT TOP 10 * FROM Person.Person";
             ResultSet rs = stmt.executeQuery(SQL);
 
             // Solucion para iterar y mostrar los resultados del query.
